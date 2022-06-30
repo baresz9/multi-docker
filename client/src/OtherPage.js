0ee5ff1b75b203f9dc7376af6f21406721c5fb98
baresz9 <alkee97@gmail.com>
2022-06-30 09:46:37 +0200

    adding everything for the 1st time

---
+++ client/src/OtherPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const test = () => {
+  return (
+    <div>
+      Im some other page!
+      <Link to="/">Go back home</Link>
+    </div>
+  );
+};
+export default test;
